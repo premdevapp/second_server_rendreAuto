00d797e0764da83a956220f28a198c5946af18ca
premnath <premdevapp@gmail.com>
2021-12-04 11:39:19 +0530

    change in data endpoint

--- server/routes/collection.js
+++ server/routes/collection.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+const data = require("../data/imgageData.json");
+
+
+router.get("/", (req, res) => {
+  const intermediate = data.link.map(item => {
+      return item.slice(1);
+  });
+res.json(intermediate);
+});
+
+
 router.get("/:collection", async (req, res) => {
   try {
     const response = await axios.get(
@@ -14,6 +25,7 @@ router.get("/:collection", async (req, res) => {
         },
       }
     );
+    console.log(response.data);
     const {primary_asset_contracts, traits, stats, discord_url, external_url, image_url, twitter_username, instagram_username} = response.data.collection;
     res.json({primary_asset_contracts, traits, stats, discord_url, external_url, image_url, twitter_username, instagram_username});
   } catch (error) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,5 @@ const router = require('express').Router();
 router.use('/assets', require('./assets'));
 router.use('/collection', require('./collection'));
 
+
 module.exports = router;
