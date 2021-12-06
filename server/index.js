9ceb3300a68424af63f96fa66a9c2edf843fecaf
dev engineering <dev@five2one.com.au>
2021-12-06 14:02:52 +0000

    index.js edited online with Bitbucket

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 // env
 
 const isProdOrDev = process.env.NODE_ENV === 'development';
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(require('morgan')('dev'));
