78022f04ba8689ae6c7499b370b1e7a2805573d2
premnath <premdevapp@gmail.com>
2021-12-06 13:01:31 +0530

    new asset modification

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/:address/:id", async (req, res) => {
+router.get("/:address", async (req, res) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 12;
+  const ids = Array.from(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    (v) => v + (page - 1) * limit
+  );
+
   try {
     const response = await axios.get(
-      `https://api.opensea.io/api/v1/asset/${req.params.address}/${req.params.id}`,
+      // `https://api.opensea.io/api/v1/asset/${req.params.address}/${req.params.id}`,
+      `https://api.opensea.io/api/v1/assets?token_ids=${ids[0]}&token_ids=${ids[1]}&token_ids=${ids[2]}&token_ids=${ids[3]}&token_ids=${ids[4]}&token_ids=${ids[5]}&token_ids=${ids[6]}&token_ids=${ids[7]}&token_ids=${ids[8]}&token_ids=${ids[9]}&token_ids=${ids[10]}&token_ids=${ids[11]}&asset_contract_address=0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb&order_direction=desc&offset=${limit-12}`,
       {
         headers: {
           Authorization: {
@@ -14,9 +22,8 @@ router.get("/:address/:id", async (req, res) => {
         },
       }
     );
-    
-    const {token_id, image_url, name, owner, traits, permalink} = response.data;
-    res.json({token_id, image_url, name, owner, traits, permalink});
+    console.log(response.data);
+    res.json(response.data);
   } catch (err) {
     console.log(err);
   }
--- server/routes/collection.js
+++ server/routes/collection.js
@@ -6,12 +6,13 @@ const data = require("../data/imgageData.json");
 
 
 router.get("/", (req, res) => {
+  const page = req.query.page || 1
+  const limit = req.query.limit || 20
   const intermediate = data.link.map(item => {
       return item.slice(1);
   });
-res.json(intermediate);
+res.json(intermediate.splice((page -1)*limit,limit));
 });
-
 
 router.get("/:collection", async (req, res) => {
   try {
@@ -25,7 +26,7 @@ router.get("/:collection", async (req, res) => {
         },
       }
     );
-    console.log(response.data);
+    
     const {primary_asset_contracts, traits, stats, discord_url, external_url, image_url, twitter_username, instagram_username} = response.data.collection;
     res.json({primary_asset_contracts, traits, stats, discord_url, external_url, image_url, twitter_username, instagram_username});
   } catch (error) {
