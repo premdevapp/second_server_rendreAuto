4b0d4fcaee54ff9fc03754a3feb756f57babe13c
premnath <premdevapp@gmail.com>
2021-12-14 15:59:04 +0530

    added endpoint

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,18 +1,36 @@
 const router = require("express").Router();
 const collection = require("../model/collection");
 
-router.get("/collection", async (req, res) => {
+router.get("/collection/desc", async (req, res) => {
   try {
-     await collection.find({}).sort([['stats.total_volume', 'desc']]).exec((err, docs)=>{
-        if(err){
-            console.log(err);
+    await collection
+      .find({})
+      .sort([["stats.total_volume", "desc"]])
+      .exec((err, docs) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json({ ...docs });
         }
-        else{
-           // console.log("DOCS : ",docs);
-            res.json({data: docs});
-        }
-    });
+      });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
+router.get("/collection/popular", async (req, res) => {
+  try {
+    await collection.find({})
+      .sort([["stats.market_cap", "desc"]])
+      .limit(12)
+      .exec( (err, docs) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // console.log("DOCS : ",docs);
+          res.json({...docs });
+        }
+      })
   } catch (e) {
     console.log(e);
   }
